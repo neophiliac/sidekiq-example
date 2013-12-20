6ac523237b05975c7d875eae629e2b7408757941
Kurt Sussman <kls@merlot.com>
2013-12-20 16:49:32 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SidekiqExample::Application.config.secret_key_base = 'a3a43568727a8c959a8b545cb08620c1f508e923be0785f4ee4f1ba8d34c5082ec5fba578d9c81211e5b4a96af5de753afa5608b3cc4193a4c0b32ba32e304e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SidekiqExample::Application.config.session_store :cookie_store, key: '_sidekiq-example_session'
