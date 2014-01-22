96a620003d4bd9c63441a6993cdd732df7a22f3a
Kurt Sussman <kls@merlot.com>
2014-01-22 14:44:52 +0100

    Don't send email from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-
-  def create
-    # send email to params['addr']
-    @mail = Notification.too_fast({:addr => email_addr_param})
-  end
 end
